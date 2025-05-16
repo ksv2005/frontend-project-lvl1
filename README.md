b9bd7831598a5b6a0db55fa6fce1a5e49043b42c
Sergey <sergey@kochegarov.me>
2025-05-16 13:46:43 +0300

    Настройка линтера и исправление ошибок

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/ksv2005/frontend-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/ksv2005/frontend-project-lvl1/actions)
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=ksv2005_frontend-project-lvl1&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=ksv2005_frontend-project-lvl1)
--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,8 @@
-import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync'
 
 export default () => {
-    console.log('Welcome to the Brain Games!');
-
-    const name = readlineSync.question('May I have your name? ');
-    const answer = `Hello, ${name}!`
-    console.log(answer);
+  console.log('Welcome to the Brain Games!')
+  const name = readlineSync.question('May I have your name? ')
+  const answer = `Hello, ${name}!`
+  console.log(answer)
 }
